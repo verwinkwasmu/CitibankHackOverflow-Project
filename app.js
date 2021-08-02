d89c88c92fb49786d57602f4e6d88e3d27086bba
verwinkwasmu <verwin.kwa.2019@sis.smu.edu.sg>
2021-08-02 09:33:13 -0700

    app setting updates and demo dont delete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path')
 
 // Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
 
@@ -17,6 +18,15 @@ const connectDB = require('./config/db');
 // Load Config
 dotenv.config({path: './config/config.env'})
 
+
+// load assets
+// DONT DELETE will need this once frontend is completed
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname + "/views/demoRoute.html");
+// })
+// app.get('/index2', (req,res)=>{
+//     res.sendFile(__dirname + "/views/demoRoute2.html");
+// })
 // Connect to DB
 connectDB();
 
@@ -28,4 +38,6 @@ app.use('/', require('./routes/libraryRoute'))
 
 
 // port hosted on 3000 (access via localhost:3000)
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Application started and listening on port 3000')
+});
