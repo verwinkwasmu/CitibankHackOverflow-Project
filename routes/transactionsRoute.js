457abace4d0855ea41611db3967b875c3118232e
jazlynnTay <jazlynntay@gmail.com>
2021-08-03 22:06:25 +0800

    Update transactionsRoute.js

--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -12,6 +12,30 @@ router.get('/transactions', async (req,res) => {
         res.json({message: err})
     }
 })
+
+
+//add new transaction
+router.post('/transactions/add', async (req,res) => {
+    const transaction = new Transaction({
+        voucherID: req.body.voucherID,
+        voucherName: req.body.voucherName,
+        price: req.body.price,
+        value: req.body.value,
+        expiryDuration: req.body.expiryDuration,
+        totalAmountPaid: req.body.totalAmountPaid,
+        totalAmountSaved: req.body.totalAmountSaved,
+        transactionID: req.body.transactionID,
+        originalValueOfProduct: req.body.originalValueOfProduct,
+       
+    });
+    try{
+        const savedTransaction = await transaction.save();
+        res.json(savedTransaction)
+    } catch(err){
+        res.json({message: err})
+    } 
+});
+
 // // Create product
 // router.post('/api/products/add', (req,res) => {
 //     const product = new Product({
